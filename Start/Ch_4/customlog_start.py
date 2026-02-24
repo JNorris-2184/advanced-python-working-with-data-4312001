9eaf2a4b818770e5701d1c7841291a36c92d333d
JDN-Spark <jnorris2575@conestogac.on.ca>
2026-02-24 14:59:45 -0500

    Ch3 - the rest

--- Start/Ch_4/customlog_start.py
+++ Start/Ch_4/customlog_start.py
@@ -3,13 +3,21 @@
 import logging
 
 # TODO: add another function to log from
-
+def another_function():
+    logging.debug("This is a debug level log message", extra=extdata)
 
 # set the output file and debug level, and
 # TODO: use a custom formatting specification
-logging.basicConfig(filename="output.log",
-                    level=logging.DEBUG)
+fmtstr = "User: %(user)s %(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d %(message)s"
+datestr = "%m/%d/%Y %I:%M:%S %p"
+extdata = {"user":"me"}
 
-logging.info("This is an info-level log message")
-logging.warning("This is a warning-level message")
+logging.basicConfig(filename="output.log",
+                    level=logging.DEBUG,
+                    format = fmtstr,
+                    datefmt = datestr)
+
+# logging.info("This is an info-level log message")
+# logging.warning("This is a warning-level message")
+another_function()
 
